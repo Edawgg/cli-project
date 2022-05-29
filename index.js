08045d215e9f3cfdc9d684752a15cff49cf3b59d
Edawgg <99540833+Edawgg@users.noreply.github.com>
2022-05-29 17:09:26 -0500

    break time

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
+import inquirer from '../lib/node_modules/inquirer/package.json';
+
+const questions = [
+    {
+        type: 'input',
+        name: 'your_name',
+        message: 'What is your name'
+    },
+    {
+        type: 'id',
+        name: 'identification_number',
+        message: 'Add numeric value to organize profiles'
+    },
+    {
+        type: 'email',
+        name: 'email_address',
+        message: 'enter email address'
+    }
+]
+
+
